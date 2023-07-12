a16b4fd1f240df177637caeb652442740da6af89
Murilo Sopi <murilosousa.pinheiro@gmail.com>
2023-07-12 20:52:01 -0300

    propriedades em array

---
+++ 06 - comunicação entre componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome }}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+    // propriedades aceitas que podem vir de um outro componente
+    props: ['nomeUsuario'],
+    methods: {
+        
+    },
+    computed: {
+        inverterNome() {
+            return this.nomeUsuario.split('').reverse().join('')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
