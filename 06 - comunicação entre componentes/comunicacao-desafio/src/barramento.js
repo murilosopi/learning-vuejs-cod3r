3a9696904372ba6fe75d446f654e5492d74a677f
Murilo Sopi <murilosousa.pinheiro@gmail.com>
2023-07-15 10:26:40 -0300

    desafio de comunicação entre componentes

---
+++ 06 - comunicação entre componentes/comunicacao-desafio/src/barramento.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+export default new Vue({
+    methods: {
+        selecionarUsuario(usuario) {
+            this.$emit('usuarioSelecionado', usuario);
+        },
+        quandoUsuarioSelecionado(callback) {
+            this.$on('usuarioSelecionado', callback);
+        }
+    }
+});
