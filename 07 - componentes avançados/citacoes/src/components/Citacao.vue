ff3bc33f74227491b1f463f323850342b9af6fe1
Murilo Sopi <dev.murilosopi@gmail.com>
2023-07-18 19:59:18 -0300

    slots com e sem name

---
+++ 07 - componentes avançados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="citacao">
+        <div class="row">
+            <slot name="autor"></slot>
+            <slot name="fonte"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    h6 {
+        font-weight: normal;
+        font-style: italic;
+    }
+
+    .row {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
