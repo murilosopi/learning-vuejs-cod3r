173ceb4ce12cc17bcce618012ad2471249e6b77c
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-19 10:55:34 -0300

    animações e transições

---
+++ 11 - animações/animacoes-exercicios/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
+
+		<!-- type: vai definir qual é a propriedade que vai mandar no tempo para retirada da DOM -->
+		<!-- appear: inicia a animação no carregamento do componente -->
+		<transition name="slide" type="animation" appear>
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+
+		<!-- classes personalizadas -->
+		<transition 
+			enter-active-class="animated bounce"
+			leave-to-class="animated shake">
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+
+		<hr>
+
+		<b-select v-model="tipoAnimacao">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+
+		<transition :name="tipoAnimacao" mode="out-in">
+			<b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
+			<b-alert variant="warning" show v-else key="aviso">{{ msg }}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Uma mensagem de informação para o usuário!",
+      exibir: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 2s;
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+		transform: translateY(0);
+  }
+}
+
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 1s ease;
+  transition: opacity .7s;
+
+}
+
+.slide-leave-active {
+  animation: slide-out 1s ease;
+  transition: opacity .7s;
+}
+</style>
