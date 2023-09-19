66b60d52625186a919ffd6368d7e5e61d5612e72
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-19 16:07:15 -0300

    Vuex: state, store, getters e mutations

---
+++ 14 - vuex/vuex-exercicios/src/components/Resumo.vue
@@ -0,0 +1,41 @@
+<template>
+    <Painel titulo="Resumo" roxo>
+        <div class="resumo">
+            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+            <hr>
+            <button>Finalizar!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    // getters via this.$store
+
+    // computed: {
+    //     total() {
+    //         return this.$store.getters.valorTotal;
+    //     }
+    // }
+
+    // getters via mapGetter com array
+    // computed: mapGetters(['valorTotal'])
+
+    // getters via mapGetter com objeto
+    computed: mapGetters({
+        total: 'valorTotal'
+    })
+}
+</script>
+
+<style>
+    table {
+        width: 100%;
+    }
+
+    td {
+        border-top: 1px solid #EEE;
+        width: 33%;
+    }
+</style>
